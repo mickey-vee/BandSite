33026578f1966b83ab8aa191ac37f7fa69b79f8b
mickey-vee <mvoong27@gmail.com>
2024-06-27 16:33:46 -0400

    Link script to bio page and add comment array

---
+++ scripts/index-page.js
@@ -0,0 +1,22 @@
+const commentArray = [
+  {
+    Name: "Victor Pinto",
+    Date: "11/02/2023",
+    Comment:
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  },
+  {
+    Name: "Christina Cabrera",
+    Date: "10/28/2023",
+    Comment:
+      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    Name: "Isaac Tadesse",
+    Date: "10/20/2023",
+    Comment:
+      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  },
+];
+
+console.log(commentArray);
