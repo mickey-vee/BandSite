c69403bf71246ecf8f5ad5c5161665b460e04f08
mickey-vee <mvoong27@gmail.com>
2024-07-01 16:32:25 -0400

    Fix css styling for bio and show. Fix JS for comments

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -4,28 +4,25 @@ const commentArray = [
     Date: "11/02/2023",
     Comment:
       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+    ImageClass: "comments__old-image",
   },
   {
     Name: "Christina Cabrera",
     Date: "10/28/2023",
     Comment:
       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+    ImageClass: "comments__old-image",
   },
   {
     Name: "Isaac Tadesse",
     Date: "10/20/2023",
     Comment:
       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+    ImageClass: "comments__old-image",
   },
 ];
 
-/* function to create class */
-
-const elementClassAppend = (element, className, parentElement) => {
-  const newElement = document.createElement(element);
-  newElement.classList.add(className);
-  parentElement.appendChild(newElement);
-};
+const commentSection = document.querySelector(".form-comments");
 
 /* function to create new element, add class name, add text and append */
 const elementClassTextAppend = (element, className, text, parentElement) => {
@@ -42,21 +39,16 @@ let mm = String(today.getMonth() + 1).padStart(2, "0");
 let yyyy = today.getFullYear();
 today = mm + "/" + dd + "/" + yyyy;
 
-/* form function */
-document.addEventListener("DOMContentLoaded", (event) => {
-  const form = document.getElementById("form");
-
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    const name = document.getElementById("name").value;
-    const comment = document.getElementById("comment").value;
-
+/* function for comments */
+const renderComments = () => {
+  const commentSection = document.querySelector(".form-comments");
+  commentSection.innerHTML = "";
+  commentArray.forEach((comment) => {
     const comments = document.createElement("div");
     comments.classList.add("comments__wrapper");
 
     const commentImage = document.createElement("div");
-    commentImage.classList.add("comments__image");
+    commentImage.classList.add(comment.ImageClass);
     comments.appendChild(commentImage);
 
     const commentOld = document.createElement("div");
@@ -67,54 +59,61 @@ document.addEventListener("DOMContentLoaded", (event) => {
     commentNameDate.classList.add("comments__name-date");
     commentOld.appendChild(commentNameDate);
 
-    elementClassTextAppend("h4", "comments__old-name", name, commentNameDate);
-    elementClassTextAppend("h4", "comments__old-date", today, commentNameDate);
-    elementClassTextAppend("p", "comments__old-text", comment, commentOld);
-
-    const formSection = document.querySelector(".form-comments");
-
-    formSection.insertBefore(comments, commentSection.firstChild);
-
-    form.reset();
+    elementClassTextAppend(
+      "h4",
+      "comments__old-name",
+      comment.Name,
+      commentNameDate
+    );
+    elementClassTextAppend(
+      "h4",
+      "comments__old-date",
+      comment.Date,
+      commentNameDate
+    );
+    elementClassTextAppend(
+      "p",
+      "comments__old-text",
+      comment.Comment,
+      commentOld
+    );
+    commentSection.appendChild(comments);
   });
-});
+};
 
-const commentSection = document.querySelector(".form-comments");
+/* form function */
+document.addEventListener("DOMContentLoaded", (event) => {
+  const form = document.getElementById("form");
+  const nameInput = document.querySelector(".comments__name");
+  const commentInput = document.querySelector(".comments__text");
+  const formSection = document.querySelector(".form-comments");
 
-commentArray.forEach(function (comment) {
-  const comments = document.createElement("div");
-  comments.classList.add("comments__wrapper");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const name = nameInput.value.trim();
+    const comment = commentInput.value.trim();
 
-  const commentImage = document.createElement("div");
-  commentImage.classList.add("comments__old-image");
-  comments.appendChild(commentImage);
+    nameInput.classList.remove("comments__required");
+    commentInput.classList.remove("comments__required");
 
-  const commentOld = document.createElement("div");
-  commentOld.classList.add("comments__old-wrapper");
-  comments.appendChild(commentOld);
-
-  const commentNameDate = document.createElement("div");
-  commentNameDate.classList.add("comments__name-date");
-  commentOld.appendChild(commentNameDate);
-
-  elementClassTextAppend(
-    "h4",
-    "comments__old-name",
-    comment.Name,
-    commentNameDate
-  );
-  elementClassTextAppend(
-    "h4",
-    "comments__old-date",
-    comment.Date,
-    commentNameDate
-  );
-  elementClassTextAppend(
-    "p",
-    "comments__old-text",
-    comment.Comment,
-    commentOld
-  );
-
-  commentSection.appendChild(comments);
+    if (name === "" && comment === "") {
+      nameInput.classList.add("comments__required");
+      commentInput.classList.add("comments__required");
+    } else if (name === "") {
+      nameInput.classList.add("comments__required");
+    } else if (comment === "") {
+      commentInput.classList.add("comments__required");
+    } else {
+      const newComment = {
+        Name: name,
+        Date: today,
+        Comment: comment,
+        ImageClass: "comments__image",
+      };
+      commentArray.unshift(newComment);
+      renderComments();
+      form.reset();
+    }
+  });
+  renderComments();
 });
